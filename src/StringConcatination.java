0032e762e334b40650122d1d4caa994a24b2269d
sanjanamalkar <sanjanamalkar@gmail.com>
2023-03-06 12:24:10 +0530

    Added:concatination of two strings

---
+++ src/StringConcatination.java
@@ -0,0 +1,8 @@
+public class StringConcatination {
+    public static void main(String[] args) {
+        String str1="Hello Guys ";
+        String str2="Welcome to Bridgelabz...";
+        String output=str1+str2;
+        System.out.println(output);
+    }
+}
